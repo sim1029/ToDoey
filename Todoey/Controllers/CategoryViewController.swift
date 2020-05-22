96d10649cdf163e3cd033daad710d498ceee2bd7
Simon Schueller <simonschueller@Simons-MBP.lan>
2020-05-22 12:47:09 -0400

    Setup category and todo views and segue

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -31,6 +31,19 @@ class CategoryViewController: UITableViewController {
         return cell
     }
     
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow{
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
     //MARK: - Data Manipulation Methods
     func saveCategories(){
         do{
@@ -68,7 +81,7 @@ class CategoryViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    //MARK: - TableView Delegate Methods
+    
     
     
 
